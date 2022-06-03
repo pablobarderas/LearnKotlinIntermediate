e118713ffe04db592781c6516f88ddfaf35926aa
pablobarderas <pbarderas31@gmail.com>
2022-06-03 04:51:28 +0200

    Lesson one Enum Classes: completed

--- app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
@@ -1,5 +1,45 @@
 package com.example.learnkotlinintermediate
 
-fun main(){
-    println("Hola android")
-}
+    fun main(){
+
+        // Lesson one: Enum Classes with integer associated
+        enumClasses()
+    }
+
+    // Enum Classes
+    enum class Direction(val dir : Int) {
+
+        NORTH(1), SOUTH(-1), EAST(1), WEST(-1);
+
+        fun description(): String {
+
+            return when (this) {
+                NORTH -> "North direction"
+                SOUTH -> "South direction"
+                else -> "We don't know the direction"
+            }
+        }
+    }
+
+    private fun enumClasses(){
+
+        // Value assignments
+        // With this symbol -> ? , i indicate that the variable can be null
+        var userDirection: Direction? = null
+        println("Direction: $userDirection")
+
+        userDirection = Direction.SOUTH
+        println("Direction: $userDirection")
+
+        // Defect properties
+        println("Propety name: ${userDirection.name}")
+
+        // Valor position
+        println("Propety ordinal: ${userDirection.ordinal}")
+
+        // Functions
+        println(userDirection.description())
+
+        // Initialization
+        println("$userDirection pos: ${userDirection.dir}")
+    }
