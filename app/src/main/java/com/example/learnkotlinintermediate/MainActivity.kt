1ccf1d0b2007bdc0b0e628d1bed32b69ebb91358
pablobarderas <pbarderas31@gmail.com>
2022-06-03 04:23:31 +0200

    Main changed

--- app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
+++ app/src/main/java/com/example/learnkotlinintermediate/MainActivity.kt
@@ -1,11 +1,5 @@
 package com.example.learnkotlinintermediate
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+fun main(){
+    println("Hola android")
 }
